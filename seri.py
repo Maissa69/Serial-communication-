6bc21b93eb4937102eb2e000d052ee8e3f5fced1
Maissa <maissa@localhost.localdomain>
2024-10-05 05:37:25 +0200

    Modifications de seri.py et sketch_sep21a.ino

--- seri.py
+++ seri.py
@@ -5,10 +5,10 @@ import time
 ser = serial.Serial('/dev/ttyUSB0', 9600)
 time.sleep(2)  # Attendre que la connexion soit établie
 
-ser.write(b'F')  # Envoyer une commande pour avancer
-time.sleep(5)    # Attendre 5 secondes
-ser.write(b'B')  # Envoyer une commande pour stopper
-time.sleep(5)
+ser.write(b'Avancer')  # Envoyer une commande pour avancer
+#time.sleep(5)    # Attendre 5 secondes
+ser.write(b'Droite')  # Envoyer une commande pour tourner a droite 
+#time.sleep(5)
 ser.write(b'S')
 ser.close()  # Fermer la connexion série
 
--- sketch_sep21a.ino
+++ sketch_sep21a.ino
@@ -1,28 +1,66 @@
-#define IN1 9
-#define IN2 10
-#define ENA 11
+#define INA1 8
+#define INA2 9
+#define ENAA 3
+
+#define INB1 10
+#define INB2 11
+#define ENAB 4
 
 void setup() {
     Serial.begin(9600); // Initialiser la communication série
-    pinMode(IN1, OUTPUT);
-    pinMode(IN2, OUTPUT);
-    pinMode(ENA, OUTPUT);
+    pinMode(INA1, OUTPUT);
+    pinMode(INA2, OUTPUT);
+    pinMode(ENAA, OUTPUT);
+    pinMode(INB1, OUTPUT);
+    pinMode(INB2, OUTPUT);
+    pinMode(ENAB, OUTPUT);
 }
 
 void loop() {
     if (Serial.available()) {
-        char command = Serial.read();
-        if (command == 'F') { // Avancer
-            digitalWrite(IN1, HIGH);
-            digitalWrite(IN2, LOW);
-            analogWrite(ENA, 150); // Vitesse maximale
-        } else if (command == 'B') { // Reculer
-            digitalWrite(IN1, LOW);
-            digitalWrite(IN2, HIGH);
-            analogWrite(ENA, 150); // Vitesse maximale
-        } else if (command == 'S') { // Stopper
-            digitalWrite(IN1, LOW);
-            digitalWrite(IN2, LOW);
+        String command = Serial.readStringUntil('\n'); 
+        
+        if (command == "Avancer") { 
+            avancer(150);
+
+        } else if (command == "Droite") { 
+            droite();
+
+        } else if (command == "Gauche") { 
+            gauche(); 
+
+        } else if (command == "Stope") { 
+            stope();
         }
     }
+}
+
+void avancer(int vitesse) {
+    digitalWrite(INA1, HIGH);
+    digitalWrite(INB1, HIGH);
+    digitalWrite(INA2, LOW);
+    digitalWrite(INB2, LOW);
+    analogWrite(ENAA, vitesse); 
+    analogWrite(ENAB, vitesse); 
+}
+
+void droite() {
+    digitalWrite(INA1, HIGH);
+    digitalWrite(INB1, LOW);
+    digitalWrite(INA2, LOW);
+    digitalWrite(INB2, LOW);
+}
+
+void gauche() { 
+    digitalWrite(INA1, LOW);
+    digitalWrite(INB1, HIGH);
+    digitalWrite(INA2, LOW);
+    digitalWrite(INB2, LOW);
+}
+
+void stope() {
+    digitalWrite(INA1, LOW);
+    digitalWrite(INB1, LOW);
+    digitalWrite(INA2, LOW);
+    digitalWrite(INB2, LOW);
 }
